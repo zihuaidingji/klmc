496599aedbc399dd360f35994795cd15b020b120
jasonrlz <44185548+jasonrlz@users.noreply.github.com>
2018-12-06 13:02:47 +0800

    Add files via upload

--- klmc/ftest.py
+++ klmc/ftest.py
@@ -32,19 +32,33 @@ def FaceTimeMark(videoPath, skipFrames=30):
                 print('EOF---------------------------------')
                 break
 
+def findOne(vcpin, vcpout, theOne):
+
+    with open(vcpin, 'r') as f1:
+        with open(vcpout, 'w') as f2:          
+            f2.write(f1.readline())#lz: 复制了VCP文件的第一行即文件地址和名字
+            for line in f1.readlines():#lz: 把f1文件的每一行逐行读取 把找到theOne的行 复制到f2文件
+                if line.find('\'{}\''.format(theOne)) != -1 :
+                    f2.write(line)
+
+               
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("-f", "--filename", help="视频文件")
     ap.add_argument("-s", "--skips", type=int, default=30, help="间隔多少帧扫描一帧")
-  
+    ap.add_argument("-o", "--theOne", type=str, default='', help="人名")    
     args = vars(ap.parse_args())
     fileName = args["filename"]
     skips = args["skips"]
-
+    theOne = args["theOne"]
     print(os.getcwd())
     print('start /b python ./klmc/ftest.py -f {} -s {}'.format(fileName, skips))
     print('Tip:后台处理完成后，项目文件保存在{}.vcp'.format(fileName))
                                                        
     #FaceTimeMark(r'C:\Users\Jason\Desktop\ozmartian-vidcutter-784b029\vidcutter\klmc\office2.mp4', 100)
     FaceTimeMark(fileName, skips)
+    if theOne != '':
+        vcpout = fileName + theOne + '.vcp'
+        findOne(fileName + '.vcp',vcpout , theOne)
+    
