2701b3440e06787c48027c1e99b4befd85430530
jasonrlz <44185548+jasonrlz@users.noreply.github.com>
2018-12-11 12:15:51 +0800

    Add files via upload

--- klmc/ftest.py
+++ klmc/ftest.py
@@ -40,8 +40,58 @@ def findOne(vcpin, vcpout, theOne):
             for line in f1.readlines():#lz: 把f1文件的每一行逐行读取 把找到theOne的行 复制到f2文件
                 if line.find('\'{}\''.format(theOne)) != -1 :
                     f2.write(line)
+                    
+def timeStats(vcpin, cvsout):
+    #输入人脸识别出指定的员工
+    name=[]
+    with open(vcpin, 'r') as f1:
+        with open(cvsout, 'w') as f2:          
+            f2.write(f1.readline())#lz: 复制了VCP文件的第一行即文件地址和名字
+            for line in f1.readlines():#lz: 把f1文件的每一行逐行读取 把找到theOne的行 复制到f2文件
+                s = line.find('[\'')
+                e = line.find('\'', s + 2)
+                name.append(line[s + 2 : e])
+                while True:
+                    s = line.find(', \'', e)
+                    
+                    if s != -1:
+                        e = line.find('\'', s + 3)
+                        name.append(line[s + 3 :e])
+                    else:
+                        break
+            print(name)
+            name2=[]#录入所有人脸识别到的人到excel
+            i = 0
+            nameCount = 0
+            for i in range(0, len(name)):
+                j = 0
+                for j in range(0, len(name2)):
+                    if name[i] == name2[j]:
+                        break
+                    else:
+                        j += 1
+                if j == len(name2):
+                    name2.append(name[i])
+                i += 1
+            print(name2)
+            a = 0#录入所有人脸识别到的人分别出现的次数到excel
+            for a in range(0, len(name2)):
+                nameCount = 0
+                b = 0
+                for b in range(0, len(name)):
+                    if name[b] == name2[a]:
+                        nameCount += 1
+                    b += 1
+                print(name2[a], nameCount)
+                f2.write(name2[a] + ',' + str(nameCount)+ '\n')
+                if a != len(name2):
+                    a += 1
 
-               
+
+                
+                
+                
+                 
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
@@ -55,10 +105,12 @@ if __name__ == '__main__':
     print(os.getcwd())
     print('start /b python ./klmc/ftest.py -f {} -s {}'.format(fileName, skips))
     print('Tip:后台处理完成后，项目文件保存在{}.vcp'.format(fileName))
+    print('Tip:后台处理完成后，统计文件保存在{}timeStats.csv'.format(fileName))
                                                        
     #FaceTimeMark(r'C:\Users\Jason\Desktop\ozmartian-vidcutter-784b029\vidcutter\klmc\office2.mp4', 100)
     FaceTimeMark(fileName, skips)
     if theOne != '':
         vcpout = fileName + theOne + '.vcp'
         findOne(fileName + '.vcp',vcpout , theOne)
-    
+    csvout = fileName + 'timeStats.csv'
+    timeStats(fileName + '.vcp', csvout)
